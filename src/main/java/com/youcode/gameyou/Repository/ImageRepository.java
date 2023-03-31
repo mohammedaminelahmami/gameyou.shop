90818e6fe91d60f9c01e865e345917795c44137a
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2023-03-31 00:11:32 +0000

    add image crud

--- src/main/java/com/youcode/gameyou/Repository/ImageRepository.java
+++ src/main/java/com/youcode/gameyou/Repository/ImageRepository.java
@@ -1,9 +1,13 @@
 package com.youcode.gameyou.Repository;
 
 import com.youcode.gameyou.Entity.Image;
+import com.youcode.gameyou.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findAllByProduct(Product product);
 }
